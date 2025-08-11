19a5346ccaae9fd2a0c2c8038d294a02b6cc0a34
Diana Tumanian <dianatumanian@gmail.com>
2025-08-11 14:23:40 +0400

    Kafka Helm done

--- exchange-generator/src/main/java/com/dianatuman/practicum/generator/ExchangeGeneratorApplication.java
+++ exchange-generator/src/main/java/com/dianatuman/practicum/generator/ExchangeGeneratorApplication.java
@@ -2,12 +2,12 @@ package com.dianatuman.practicum.generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ExchangeGeneratorApplication {
 
     public static void main(String[] args) {
