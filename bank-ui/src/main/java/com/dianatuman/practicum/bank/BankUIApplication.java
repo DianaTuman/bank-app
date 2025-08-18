78bb80783b7a98f0555f10ac30e1e32069e486b9
Diana Tumanian <dianatumanian@gmail.com>
2025-08-18 11:45:01 +0400

    Zipkin start

--- bank-ui/src/main/java/com/dianatuman/practicum/bank/BankUIApplication.java
+++ bank-ui/src/main/java/com/dianatuman/practicum/bank/BankUIApplication.java
@@ -3,9 +3,6 @@ package com.dianatuman.practicum.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,6 +11,5 @@ public class BankUIApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankUIApplication.class, args);
     }
-
 
 }
